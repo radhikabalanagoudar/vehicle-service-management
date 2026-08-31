e50c5f93d9635d65cbf298d1c3f8f4dce887f54c
radhika <radhikabalanagoudar@gmail.com>
2026-08-31 23:12:58 +0530

    Update backend server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/database");
@@ -12,6 +13,7 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
